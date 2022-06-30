cc5def2dda620f995011b091036de5c7b7dd15a4
ChristianMainarti <christian.dias@sagatechbrasil.com.br>
2022-06-30 14:54:47 -0300

    Adicionando departamentos, e controle/view de vendedores

---
+++ API_Vue/Controllers/VendedoresController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Vue.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
